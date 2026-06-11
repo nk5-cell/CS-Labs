069c10a8ad12ff59aeccb803d7e0ed76b3c1d35e
nk5-cell <253822326+nk5-cell@users.noreply.github.com>
2026-06-11 08:01:09 -0700

    Add file description and comments to carValueMain.cpp
    Added file description and comments for clarity.

--- carValueMain.cpp
+++ carValueMain.cpp
@@ -1,3 +1,11 @@
+// LAB: Car Value
+/*=====================================================================================
+File descrition: This is a program that displays the car's information after user 
+				 inputs of model year, purchase price, and current value. This program 
+				 uses car struct and functions declaration from carValue.h file, where 
+				 carValue.cpp file implement those function definitions from carValue.h 
+=====================================================================================*/
+
 #include <iostream>
 
 #include "carValue.h"
