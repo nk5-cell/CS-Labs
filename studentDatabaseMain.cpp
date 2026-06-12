65c54e6516cf7b8d08db20a7005fd95988113057
nk5-cell <253822326+nk5-cell@users.noreply.github.com>
2026-06-12 07:31:23 -0700

    added main in studentDatabaseMain.cpp

---
+++ studentDatabaseMain.cpp
@@ -0,0 +1,34 @@
+// LAB: Student database
+/*======================================================================================
+File descrition: This is a program that uses a struct named Student to store information 
+                 about a Student. In main() (studentDatabaseMain.cpp), open the 
+                 students.txt file and pass to the loadValues() function to read the 
+                 student data into the array. Then call the printValues() function to 
+                 print out the list of students.
+======================================================================================*/
+
+#include <iostream>
+#include <cstring>
+#include <fstream>
+
+#include "Student.h"
+using namespace std;
+
+int main() {
+ 
+   Student studentList[50];
+   int numStudents = 0;
+   ifstream inFile;
+   
+   inFile.open("students.txt");
+
+   if (inFile.fail()) {
+      cout << "File failed to open!" << endl;
+      return 1;
+   }
+   
+   loadValues(inFile,studentList,numStudents);
+   printValues(studentList, numStudents);
+   
+   return 0;
+}
